63e6f64cb67fcda3cdb456a0460cd2198d357484
Konekoya <tank158r5155@yahoo.com.tw>
2015-06-19 05:27:31 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish')
+    gutil = require('gulp-util'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-ruby-sass');
+
+gulp.task('js', function() {
+    return gulp.src('./js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish))
+        .pipe(uglify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('sass', function() {
+    return sass('./scss/style.scss', {style: 'compressed'})
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./js/*.js', ['js']);
+  gulp.watch('./scss/*.scss', ['sass']);
+ });
+
+gulp.task('default', ['js', 'sass', 'watch']);
