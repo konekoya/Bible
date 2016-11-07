300978c5960eaa838f0ce8ce365bc55c6f82ae08
Konekoya <tank158r5155@yahoo.com.tw>
2016-11-07 09:04:43 +0800

    Work on webpack build process

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,11 @@ module.exports = {
         test: /\.(es6|js)$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loader: "style!css"
       }
     ]
   },
